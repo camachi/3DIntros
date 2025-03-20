61426cc9a45130b12599e6eb398e07e5f3086045
machi <carlosintel9@gmail.com>
2025-03-20 01:55:36 -0400

    update

--- src/CartPage.jsx
+++ src/CartPage.jsx
@@ -1,7 +1,35 @@
-function CartPage()
-{
+function CartPage({list})
+{   if (!list || list.length === 0) {
     return (
-        <></>
+        <div className="CartDivContainer">
+
+            <div className="ItemContainer">
+            <div className="ItemTitleContainer">
+
+            <h1>
+                Your Cart
+            </h1>
+
+            </div>
+            
+            <h3>No items added to cart yet</h3>
+            </div>
+            
+        </div>
+    );
+}
+
+
+    return (
+        <div className="CartDivContainer">
+                <div className="ItemTitleContainer">
+
+                    <h1>
+                    Your Cart
+                    </h1>
+
+                </div>
+        </div>
     );
 }
 
---
+++ src/InCartItem.jsx
@@ -0,0 +1,11 @@
+
+function InCartItem({})
+{
+    return(
+        <div className="InCartItemContainer">
+            <h1>test</h1>
+        </div>
+    );
+}
+
+export default InCartItem;
